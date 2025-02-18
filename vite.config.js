9f960d995beeba24974a3a3c40a1a7f3973b2ffe
Tunje <simontunje@hotmail.com>
2025-02-18 22:06:26 +0100

    Deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/AimRV/', // This should match the repository name or the subdirectory in GitHub Pages
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'src/App.jsx', // Change this to your custom entry file if needed
+      },
+    },
+  },
+});
