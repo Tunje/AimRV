2dc0d388b837e61de2b47e41a96e35abbef1b778
Tunje <simontunje@hotmail.com>
2025-03-03 15:54:42 +0100

    Revert to working image paths and add proper image handling in Netlify

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,19 +16,19 @@ const Footer = () => {
                         <div className="grid-item sponsors">
                             <h3 className="footer-heading" id="sponsors-section">Sponsors</h3>
                             <div className="grid">
-                                <img src="../images/Artboard 1@4x.png" alt="Artboard" className="sponsor-logo" />
-                                <img src="../images/Camelbak_vit.svg" alt="Camelbak" className="sponsor-logo" />
-                                <img src="../images/Hedghog_vit.svg" alt="Hedghog" className="sponsor-logo" />
-                                <img src="../images/Kona_vit.png" alt="Kona" className="sponsor-logo" />
-                                <img src="../images/Mareld_svart.svg" alt="Mareld" className="sponsor-logo" />
-                                <img src="../images/ODD_white_RGB.png" alt="ODD" className="sponsor-logo" />
-                                <img src="../images/Oofos.png" alt="Oofos" className="sponsor-logo" />
-                                <img src="../images/Ortovox_svart.svg" alt="Ortovox" className="sponsor-logo" />
-                                <img src="../images/UD Logo.png" alt="UD" className="sponsor-logo" />
-                                <img src="../images/Upplev.png" alt="Upplev Norrköping" className="sponsor-logo" />
-                                <img src="../images/holdut_logo_sort_1.png" alt="Hold ut" className="sponsor-logo" />
-                                <img src="../images/h%C3%A4llmark logga sv_v.svg" alt="Hällmark" className="sponsor-logo" />
-                                <img src="../images/injinji_black.svg" alt="Injinji" className="sponsor-logo" />
+                                <img src="/images/Artboard 1@4x.png" alt="Artboard" className="sponsor-logo" />
+                                <img src="/images/Camelbak_vit.svg" alt="Camelbak" className="sponsor-logo" />
+                                <img src="/images/Hedghog_vit.svg" alt="Hedghog" className="sponsor-logo" />
+                                <img src="/images/Kona_vit.png" alt="Kona" className="sponsor-logo" />
+                                <img src="/images/Mareld_svart.svg" alt="Mareld" className="sponsor-logo" />
+                                <img src="/images/ODD_white_RGB.png" alt="ODD" className="sponsor-logo" />
+                                <img src="/images/Oofos.png" alt="Oofos" className="sponsor-logo" />
+                                <img src="/images/Ortovox_svart.svg" alt="Ortovox" className="sponsor-logo" />
+                                <img src="/images/UD Logo.png" alt="UD" className="sponsor-logo" />
+                                <img src="/images/Upplev.png" alt="Upplev Norrköping" className="sponsor-logo" />
+                                <img src="/images/holdut_logo_sort_1.png" alt="Hold ut" className="sponsor-logo" />
+                                <img src="/images/h%C3%A4llmark logga sv_v.svg" alt="Hällmark" className="sponsor-logo" />
+                                <img src="/images/injinji_black.svg" alt="Injinji" className="sponsor-logo" />
                             </div>
                         </div>
                         <div className="grid-item partners">
