6a26cac58e5a6d0e15097f49b02756f67efd7b83
Sweodin <petergustafsson910@gmail.com>
2025-04-16 12:10:23 +0200

    Sälen looks done.

---
+++ src/components/Footer-new.jsx
@@ -0,0 +1,63 @@
+{
+  /* Footer Section */
+}
+<section className="footer-section">
+  <div className="footer-container">
+    <div className="footer-top">
+      <div className="footer-left">
+        <div className="footer-logo">
+          <img src="/images/Aim_Challenge_logo_white.svg" alt="AIM Challenge" />
+        </div>
+
+        <div className="footer-info">
+          <p>Loftsdalian 108, 30129 Halmstad</p>
+          <p>
+            <a href="mailto:info@aimchallenge.com">info@aimchallenge.com</a>
+          </p>
+          <p>+46 (0) 73-81 87 54</p>
+        </div>
+
+        <div className="footer-social">
+          <a href="#" className="social-icon">
+            <i className="fa fa-instagram"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href="#" className="social-icon">
+            <i className="fa fa-twitter"></i>
+          </a>
+        </div>
+      </div>
+
+      <div className="footer-columns">
+        <div className="footer-column footer-column-center">
+          <h3 className="footer-title">INFORMATION</h3>
+          <ul className="footer-links">
+            <li>
+              <a href="#">Anmälningsvillkor</a>
+            </li>
+            <li>
+              <a href="#">Integritetspolicy</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="footer-column footer-column-right">
+          <h3 className="footer-title">NYHETSBREV</h3>
+          <p className="footer-newsletter-text">
+            Åtext om nyhetsbrev, samt en optin ruta för att fylla i sin
+            epostadress. Integration mot mailchimp.
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div className="footer-copyright">
+      <p> 2023 Copyright AIM Challenge.</p>
+    </div>
+  </div>
+</section>;
